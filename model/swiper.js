5018b31e5e424d79c7825f20a52afbfbc7d8aecd
xuanjieWang <2071985621@qq.com>
2024-12-09 17:27:14 +0800

    Initial Commit

---
+++ model/swiper.js
@@ -0,0 +1,8 @@
+const images = [
+  'http://xuanjie22.oss-cn-beijing.aliyuncs.com/blog/ppt01208.png',
+  "http://xuanjie22.oss-cn-beijing.aliyuncs.com/blog/ppt02638.png"
+];
+
+export function genSwiperImageList() {
+  return images;
+}
---
+++ services/home/home.js
@@ -0,0 +1,34 @@
+import {
+  config,
+  cdnBase
+} from '../../config/index';
+
+/** 获取首页数据 */
+function mockFetchHome() {
+  const {
+    delay
+  } = require('../_utils/delay');
+  const {
+    genSwiperImageList
+  } = require('../../model/swiper');
+  return delay().then(() => {
+    return {
+      swiper: genSwiperImageList(),
+      tabList: [{
+        text: '精选推荐',
+        key: 0,
+      }, ],
+      activityImg: `${cdnBase}/activity/banner.png`,
+    };
+  });
+}
+
+/** 获取首页数据 */
+export function fetchHome() {
+  if (config.useMock) {
+    return mockFetchHome();
+  }
+  return new Promise((resolve) => {
+    resolve('real api');
+  });
+}
